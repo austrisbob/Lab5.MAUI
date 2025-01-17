333ef45fd5b04072d712327cb8f5ee673954c036
austris1 <152059689+austris1@users.noreply.github.com>
2025-01-18 00:15:17 +0200

    Add project files.

---
+++ Lab5.MAUI/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Lab5.MAUIData.Interfaces;
+using Lab5.MAUIData.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace Lab5.MAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddScoped<IDepartmentApiClient, DepartmentApiClient>();
+            builder.Services.AddScoped<IDataRepository, ApiDataRepository>();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
